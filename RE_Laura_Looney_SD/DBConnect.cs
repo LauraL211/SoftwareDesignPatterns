6f4434b9ba95d3f05655dbcb9b8d315a44a3479c
LauraL211 <laura.looney028@gmail.com>
2024-01-23 10:53:43 +0000

    DB Connection

---
+++ RE_Laura_Looney_SD/DBConnect.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RE_Laura_Looney_SD
+{
+    internal class DBConnect
+    {
+        public const String oraDB = "Data Source = studentoracle:1521/orcl; User ID = ; Password = ;";
+    }
+}
---
+++ RE_Laura_Looney_SD/frmDBConnect.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace RE_Laura_Looney_SD
+{
+    partial class frmDBConnect
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStatus = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStatus
+            // 
+            this.btnStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btnStatus.Location = new System.Drawing.Point(248, 111);
+            this.btnStatus.Name = "btnStatus";
+            this.btnStatus.Size = new System.Drawing.Size(269, 103);
+            this.btnStatus.TabIndex = 0;
+            this.btnStatus.Text = "Change Connection Status";
+            this.btnStatus.UseVisualStyleBackColor = true;
+            this.btnStatus.Click += new System.EventHandler(this.btnStatus_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.lblStatus.Location = new System.Drawing.Point(281, 231);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(207, 18);
+            this.lblStatus.TabIndex = 1;
+            this.lblStatus.Text = "Connection is currently closed";
+            // 
+            // frmDBConnect
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnStatus);
+            this.Name = "frmDBConnect";
+            this.Text = "DB Connection";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStatus;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
